3ea1ef55d3a980de8fa94ba09e1907de6bd92927
chaitanya <chaitanya9516@gmail.com>
2021-03-27 23:34:58 +0530

    press enter it will trigger

--- terminal.js
+++ terminal.js
@@ -14,3 +14,41 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+let input = document.querySelector(".terminal_input_command");
+
+input.addEventListener("keydown", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        let terminal_val = input.value;
+        //alert(terminal_val);
+        if (terminal_val === "help") {
+            let output;
+
+            output = `<span>Some available commands are:</span><ul>
+            <li>about ......... About me</li>
+            <li>clear ......... Clear terminal log</li>
+            <li>exit .......... Exit terminal session</li>
+            <li>help .......... Showing available commands</li>
+            <li>links ......... Social media links</li>
+            <li>theme ......... Change theme of terminal</li>
+            <li>articles ...... Recent articles</li>
+            <li>projects ...... My pinned projects on GitHub</li>
+            </ul><span>Besides, there are some hidden feature, try to find it out!</span>`
+
+            var terminal = document.getElementsByClassName("terminal");
+
+
+
+            // var generation_p = document.createElement("p");
+            // let data = generation_p.innerHTML = output;
+            // terminal.innerHTML = data;
+            // let div_ter = document.createElement("div");
+            // div_ter.setAttribute("class", div_ter);
+            // div_ter.innerHTML = generation_p;
+
+
+
+        }
+    }
+});
