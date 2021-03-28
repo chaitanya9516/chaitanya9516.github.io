9143aa08e4ef28c5344420452921a83d549a9d0e
chaitanya <chaitanya9516@gmail.com>
2021-03-28 16:14:22 +0530

    take another input to terminal

--- terminal.js
+++ terminal.js
@@ -43,27 +43,25 @@ input.addEventListener("keydown", function(event) {
             generation_p.innerHTML = output;
             terminal.appendChild(generation_p);
 
-            let generate_div = document.createElement('div');
-            generate_div.classList.add('terminal_row');
-            generate_div.classList.add('terminal_log');
-
-
+            let generation_div = document.createElement('div');
+            generation_div.classList.add('terminal_row');
+            generation_div.classList.add('terminal_log');
 
             let generation_p2 = document.createElement('p');
             generation_p2.classList.add('terminal_row');
             generation_p2.classList.add('terminal_log');
 
-            let generate_span = document.createElement('span');
-            generate_span.innerHTML = 'ask@Chay:~$ ';
-            generate_span.style.color = 'rgb(100, 255, 218)';
-            terminal.appendChild(generate_span);
+            let generation_span = document.createElement('span');
+            generation_span.innerHTML = 'ask@Chay:~$ ';
+            generation_span.classList.add('terminal_user');
+            terminal.appendChild(generation_span);
 
-            let generate_input = document.createElement('input');
-            generate_input.classList.add('terminal_input_command');
-            generate_input.style.color = '#c1c8ec';
+            let generation_input = document.createElement('input');
+            generation_input.classList.add('terminal_input_command');
+            generation_input.style.color = '#c1c8ec';
             // generate_input.setAttribute('onclick', );
-            generation_p2.appendChild(generate_span);
-            generation_p2.appendChild(generate_input);
+            generation_p2.appendChild(generation_span);
+            generation_p2.appendChild(generation_input);
             terminal.appendChild(generation_p2);
 
 
