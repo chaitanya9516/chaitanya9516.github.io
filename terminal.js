8819b6b185963af182f05603c5a870d15e89e4ad
chaitanya <chaitanya9516@gmail.com>
2021-03-28 19:49:38 +0530

    terminal alomost set

--- terminal.js
+++ terminal.js
@@ -17,7 +17,6 @@ window.onclick = function(event) {
 
 function build_elements(output_val) {
 
-
     var terminal = document.getElementById("terminal");
     let generation_p = document.createElement('p');
     generation_p.classList.add('terminal_row');
@@ -25,10 +24,8 @@ function build_elements(output_val) {
     generation_p.innerHTML = output_val;
     terminal.appendChild(generation_p);
 
-
     let generation_p2 = document.createElement('p');
     generation_p2.classList.add('terminal_row');
-    generation_p2.classList.add('terminal_log');
 
     let generation_span = document.createElement('span');
     generation_span.innerHTML = 'ask@Chay:~$ ';
@@ -37,28 +34,45 @@ function build_elements(output_val) {
 
     let generation_input = document.createElement('input');
     generation_input.setAttribute('type', 'text');
+    generation_input.setAttribute('onclick', 'terminal_log()');
+    generation_input.setAttribute('id', 'txtbox');
+    generation_input.classList.add('terminal_input_command');
     generation_input.setAttribute('autocomplete', 'off');
     generation_input.setAttribute('spellcheck', 'false');
     generation_input.setAttribute('autocorrect', 'off');
-    generation_input.classList.add('terminal_input_command');
+
+    let local_terminal_data = localStorage.getItem("input_val");
+
+    var input_val_remove = document.getElementById("txtbox");
+    input_val_remove.remove()
+
+    let terminal_log = document.createElement('span');
+    terminal_log.classList.add('terminal_log');
+    terminal_log.innerHTML = local_terminal_data;
+    let terminal_row = document.getElementById("terminal_row");
+    terminal_row.appendChild(terminal_log);
 
     generation_p2.appendChild(generation_span);
     generation_p2.appendChild(generation_input);
-    terminal.insertBefore(generation_p2, terminal.childNodes[0]);
+    terminal.appendChild(generation_p2);
 
 }
 
-var input = document.querySelector(".terminal_input_command");
 
-input.addEventListener("keydown", function(event) {
 
-    if (event.keyCode === 13) {
-        let terminal_val = input.value;
-        var output;
-        //alert(terminal_val);
-        if (terminal_val === "help") {
+function terminal_log() {
 
-            output = `<span>Some available commands are:</span><ul>
+    var input = document.querySelector(".terminal_input_command");
+    input.addEventListener("keydown", function(event) {
+
+        if (event.keyCode === 13) {
+            var terminal_val = input.value;
+            localStorage.setItem("input_val", terminal_val);
+            var output;
+            //alert(terminal_val);
+            if (terminal_val === "help") {
+
+                output = `<span>Some available commands are:</span><ul>
             <li>about ............ About me</li>
             <li>clear ............ Clear terminal log</li>
             <li>exit ............. Exit terminal session</li>
@@ -69,10 +83,11 @@ input.addEventListener("keydown", function(event) {
             <li>projects ......... My pinned projects on GitHub</li>
             </ul><span>Besides, there are some hidden feature, try to find it out!</span>`
 
-        } else if (terminal_val === "about") {
-            output = "Hello, I' m Justin Maximillian Kimlim from Indonesia, a 15 y.o.junior high school student with hobbies of computer science, programming and science fiction.I enjoy making projects or even website clone.";
-        }
+            } else if (terminal_val === "about") {
+                output = "Hello, I' m Justin Maximillian Kimlim from Indonesia, a 15 y.o.junior high school student with hobbies of computer science, programming and science fiction.I enjoy making projects or even website clone.";
+            }
 
-        build_elements(output);
-    }
-});
+            build_elements(output);
+        }
+    });
+}
