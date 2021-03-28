a92c5035fc1471e4decac961462471582e75516d
chaitanya <chaitanya9516@gmail.com>
2021-03-28 16:46:26 +0530

    created build-elements function

--- terminal.js
+++ terminal.js
@@ -15,6 +15,30 @@ window.onclick = function(event) {
     }
 }
 
+function build_elements() {
+
+    let generation_p2 = document.createElement('p');
+    generation_p2.classList.add('terminal_row');
+    generation_p2.classList.add('terminal_log');
+
+    let generation_span = document.createElement('span');
+    generation_span.innerHTML = 'ask@Chay:~$ ';
+    generation_span.classList.add('terminal_user');
+    //terminal.appendChild(generation_span);
+
+    let generation_input = document.createElement('input');
+    generation_input.setAttribute('type', 'text');
+    generation_input.setAttribute('autocomplete', 'off');
+    generation_input.setAttribute('spellcheck', 'false');
+    generation_input.setAttribute('autocorrect', 'off');
+    generation_input.classList.add('terminal_input_command');
+
+    generation_p2.appendChild(generation_span);
+    generation_p2.appendChild(generation_input);
+    terminal.appendChild(generation_p2);
+
+}
+
 let input = document.querySelector(".terminal_input_command");
 
 input.addEventListener("keydown", function(event) {
@@ -36,34 +60,14 @@ input.addEventListener("keydown", function(event) {
             <li>projects ......... My pinned projects on GitHub</li>
             </ul><span>Besides, there are some hidden feature, try to find it out!</span>`
 
-            let terminal = document.getElementById("terminal");
+            var terminal = document.getElementById("terminal");
             let generation_p = document.createElement('p');
             generation_p.classList.add('terminal_row');
             generation_p.classList.add('terminal_log');
             generation_p.innerHTML = output;
             terminal.appendChild(generation_p);
 
-            let generation_div = document.createElement('div');
-            generation_div.classList.add('terminal_row');
-            generation_div.classList.add('terminal_log');
-
-            let generation_p2 = document.createElement('p');
-            generation_p2.classList.add('terminal_row');
-            generation_p2.classList.add('terminal_log');
-
-            let generation_span = document.createElement('span');
-            generation_span.innerHTML = 'ask@Chay:~$ ';
-            generation_span.classList.add('terminal_user');
-            terminal.appendChild(generation_span);
-
-            let generation_input = document.createElement('input');
-            generation_input.classList.add('terminal_input_command');
-            generation_input.style.color = '#c1c8ec';
-            // generate_input.setAttribute('onclick', );
-            generation_p2.appendChild(generation_span);
-            generation_p2.appendChild(generation_input);
-            terminal.appendChild(generation_p2);
-
+            build_elements();
 
 
         }
